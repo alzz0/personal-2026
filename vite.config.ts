08d5b8b14e3f8886db053b02de0e596b9f957c6f
Ali Mansour <alimansour@Alis-MacBook-Pro.local>
2026-02-20 23:38:20 -0700

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,10 @@ import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1] ?? ''
+const isUserOrOrgSite = repoName.endsWith('.github.io')
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/',
+  base: process.env.GITHUB_ACTIONS ? (isUserOrOrgSite ? '/' : `/${repoName}/`) : '/',
 })
