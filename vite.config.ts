f4b392f931ae08ed2efdf2d64679f4720e121c54
Ali Mansour <alimansour@Alis-MacBook-Pro.local>
2026-08-12 22:24:06 -0600

    add: engineering page

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
 import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'node:path'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   base: '/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        engineering: resolve(__dirname, 'engineering/index.html'),
+        aiClipping: resolve(__dirname, 'engineering/ai-clipping/index.html'),
+      },
+    },
+  },
 })
